928638115ffbaf8d0277534ae3a8cbb38481e042
Yehia Azanki <yazanki@gmail.com>
2023-03-13 19:37:30 -0300

    Add Security API

---
+++ money-api/src/main/java/br/com/moneyapi/security/util/GeradorSenha.java
@@ -0,0 +1,10 @@
+package br.com.moneyapi.security.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeradorSenha {
+    public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("123456"));
+	}
+}
